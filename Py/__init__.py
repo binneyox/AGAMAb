7933958c1b8fa789135c68e7a0c714ca8dce3376
binneyox <137393307+binneyox@users.noreply.github.com>
2025-12-05 17:42:07 +0000

    updates Dec 5

--- Py/__init__.py
+++ Py/__init__.py
@@ -1,5 +1,5 @@
 import os
-os.aff_dll_directory("h:/u/c/agama/agama/x64/release")
-from Py_agama import *            # import everything from the C++ library
-from Py_agama import __version__, __doc__  # these two are not automatically imported=
-del Py_agama                          # remove the C++ library from the root namespace
+os.add_dll_directory("h:/u/c/agama/agama/x64/release")
+from .agamab import *            # import everything from the C++ library
+from .agamab import __version__, __doc__  # these two are not automatically imported=
+del agamab                          # remove the C++ library from the root namespace
