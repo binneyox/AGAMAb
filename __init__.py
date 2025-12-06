45da17c0024b283db1e321a0ce6b3a6737e7c3e6
binneyox <137393307+binneyox@users.noreply.github.com>
2025-12-06 17:16:32 +0000

    Updated Dec 6

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .Py_agama import *               # import everything from the C++ library
-from .Py_agama import __version__, __doc__  # these two are not automatically imported=
-del Py_agama                          # remove the C++ library from the root namespace
+from .agamab import *               # import everything from the C++ library
+from .agamab import __version__, __doc__  # these two are not automatically imported=
+del agamab                          # remove the C++ library from the root namespace
---
+++ examples/testAFTGtwo.cpp
@@ -0,0 +1,39 @@
+/* Integrates an orbit in a flattened Hernquist potential and use both
+ * the TG action finder and the Fudge action finder to compute actions
+ * along the orbit
+ */
+
+#include "actions_newtorus.h"
+#include "actions_newisochrone.h"
+#include "potential_analytic.h"
+#include "potential_utils.h"
+#include "potential_bars.h"
+#include "potential_factory.h"
+#include "orbit.h"
+
+int main(void){
+	std::vector<double> Rs,zs,vRs,vzs,thetas,pthetas;
+	double Rmin=7, Rmax=30, Zmax=15, Vmax=.062;
+	potential::PtrPotential pot = potential::createPotential(utils::KeyValueMap(
+		"type=spheroid, gamma=1, beta=4, scaleradius=1, q=0.6"));
+	actions::TorusGenerator TG(*pot,1e-4);
+	actions::Actions J(.3,.4,1);
+	actions::Torus T(TG.fitTorus(J));
+	coord::PosVelCyl Rzv(coord::toPosVelCyl(T.from_true(actions::Angles(.4,.4,.4))));
+	double duration=80*M_PI/T.freqs.Omegar, dt=duration/(double)2000;
+	std::vector<std::pair<coord::PosVelCyl,double> >
+			traj(orbit::integrateTraj(Rzv,duration,dt,*pot));
+	for(int i=0;i<traj.size();i++){
+		Rs.push_back(traj[i].first.R); zs.push_back(traj[i].first.z);
+	}
+
+	actions::ActionFinderAxisymFudge AF(pot);
+	actions::ActionFinderTG AFTG(pot,TG,&AF);
+	printf("        TG           Fudge\n   Jr       Jz       Jr       Jz\n");
+	for(int i=0; i<traj.size(); i+=400){
+		actions::ActionAngles aa0(AFTG.actionAngles(traj[i].first));
+		actions::Actions JSt(AF.actions(traj[i].first));
+		printf("%f %f %f %f\n",aa0.Jr,aa0.Jz,JSt.Jr,JSt.Jz);
+	}
+//	printf("aa0: Jr %f Jz %f Jphi %f thetar %f thetaz %f\n",aa0.Jr,aa0.Jz,aa0.Jphi,aa0.thetar,aa0.thetaz);
+}
